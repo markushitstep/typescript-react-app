26c790f7d6b080e611351f66b32ef507cd933188
Maxim <markushitstep@gmail.com>
2022-01-06 15:10:39 +0200

    add component App and redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,44 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store'
 
-interface AppProps {
-    color?: string; // optional
-}
-
-
-const App = (props: AppProps): JSX.Element => {
-    return(
-        <div>{props.color}</div>
-    )
-};
-
-// interface AppState {
-//     counter: number; // optional
-// }
-
-// class App extends React.Component<AppProps, AppState> {
-
-//     constructor(props: AppProps) {
-//         super(props);
-
-//         this.state = { counter: 0 }
-//     }
-
-//     onIncrement = (): void => {
-//         this.setState({ counter: this.state.counter + 1 })
-//     }
-//     onDecrement = (): void => {
-//         this.setState({ counter: this.state.counter - 1 })
-//     }
-//     render() {
-//         return (
-//             <div>
-//                 <button onClick={this.onIncrement}>+</button>
-//                 <button onClick={this.onDecrement}>-</button>
-//                 {this.state.counter}
-//             </div>
-//         )
-//     }
-// }
-
-ReactDOM.render(<App color='red'/>, document.querySelector('#root'));
+ReactDOM.render(
+    <Provider store={store}
+    <App />,
+    document.querySelector('#root')
+);
